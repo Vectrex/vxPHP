6e898564c98e432ad7f62df601d18aabd36c2857
Gregor Kofler <info@gregorkofler.com>
2012-02-16 11:33:59 +0100

    Interfaces collected in separate include.
    Change-Id: I97c7ef29bf5e025f25049bbbeb5e148a5287ff80

---
+++ core/interfaces/interfaces.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * stub for more detailed interfaces in the future
+ * @author Gregor Kofler
+ */
+
+/**
+ * EventDispatcher related interfaces
+ */
+interface EventListener {
+	public function update(Subject $subject);
+}
+
+interface Subject {
+}
+
+/**
+ * webpage related interfaces
+ */
+interface siteSpecifics {
+	public function pageHeader();
+	public function pageFooter();
+	public function content();
+}
+
+/**
+ * template parser related interfaces
+ */
+interface templateSpecifics {
+	public function setSource($source);
+	public function parse();
+}
+?>
