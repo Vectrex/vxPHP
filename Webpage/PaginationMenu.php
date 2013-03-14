e9d4de835dfee7e26488ad66efde3ae283186996
gregor <git@gregorkofler.com>
2013-03-14 15:27:09 +0100

    Initial commit of PaginationMenu class

---
+++ Webpage/PaginationMenu.php
@@ -0,0 +1,42 @@
+<?php
+namespace vxPHP\Webpage;
+
+class PaginationMenu {
+
+	private	$parameterName,
+			$entriesPerPage,
+			$totalEntries,
+			$template,
+			$range;
+
+	public function __construct() {
+
+	}
+
+	public function setTotalEntries() {
+
+	}
+
+	public function setEntriesPerPage() {
+
+	}
+
+	public function setParameterName() {
+
+	}
+
+	public function setTemplate() {
+
+	}
+
+	public function setRange() {
+
+	}
+
+	public function render() {
+
+	}
+
+}
+
+?>
