3c00fb2d34938c531d01d4f1312939b7aaf93e17
gregor <git@gregorkofler.com>
2013-03-29 21:53:14 +0100

    Core functionality finalized; comments added

--- Orm/Custom/ArticleQuery.php
+++ Orm/Custom/ArticleQuery.php
@@ -5,6 +5,24 @@ use vxPHP\Orm\Custom\Article;
 use vxPHP\Orm\Custom\ArticleCategory;
 use vxPHP\Database\Mysqldbi;
 
+/**
+ * query object which returns an array of Article objects
+ *
+ * @example
+ *
+ * $articles =	vxPHP\Orm\Custom\ArticleQuery::create($db)->
+ * 				filterByCategory($myCat)->
+ * 				where('article_date < ?', new DateTime()->format('Y-m-d'))->
+ * 				sortBy('customSort', FALSE)->
+ * 				sortBy('Headline')->
+ * 				selectFirst(2);
+ *
+ * @author Gregor Kofler
+ * @version 0.1.0 2013-03-29
+ * 
+ * @todo avoid preparation of statement when underlying SQL doesn't change
+ *
+ */
 class ArticleQuery {
 
 	private	$dbConnection,
@@ -12,42 +30,68 @@ class ArticleQuery {
 			$columnSorts	= array(),
 			$valuesToBind	= array(),
 			$sql,
-			$sqlNeedsUpdate;
+			$lastQuerySql;
 
+	/**
+	 * provide initial database connection
+	 * currently only allows a Mysqli backend
+	 *
+	 * @param Mysqldbi $dbConnection
+	 */
 	public function __construct(Mysqldbi $dbConnection) {
 
-		$this->sqlNeedsUpdate = TRUE;
 		$this->dbConnection = $dbConnection;
 
 	}
 
+	/**
+	 * add appropriate WHERE clause that filters for $category
+	 *
+	 * @param ArticleCategory $category
+	 * @return ArticleQuery
+	 */
 	public function filterByCategory(ArticleCategory $category) {
 
 		$this->addCondition("articlecategoriesID = ?", $category->getId());
-		$this->sqlNeedsUpdate = TRUE;
-
 		return $this;
 
 	}
 
+	/**
+	 * add WHERE clause that filters articles where $columnName matches $value
+	 *
+	 * @param string $columnName
+	 * @param string|number $value
+	 * @return ArticleQuery
+	 */
 	public function filter($columnName, $value) {
 
 		$this->addCondition("$columnName = ?", $value);
-		$this->sqlNeedsUpdate = TRUE;
-
 		return $this;
 
 	}
 
+	/**
+	 * add an "arbitrary" WHERE clause and values to bind
+	 *
+	 * @param string $whereClause
+	 * @param array $valuesToBind
+	 * @return ArticleQuery
+	 */
 	public function where($whereClause, Array $valuesToBind = NULL) {
 
 		$this->addCondition($whereClause, $valuesToBind);
-		$this->sqlNeedsUpdate = TRUE;
-
 		return $this;
 
 	}
 
+	/**
+	 * add ORDER BY clause
+	 *
+	 * @param string $columnName
+	 * @param boolean $asc
+	 * @return ArticleQuery
+	 */
 	public function sortBy($columnName, $asc = TRUE) {
 
 		$sort = new \stdClass();
@@ -56,32 +100,82 @@ class ArticleQuery {
 		$sort->asc = !!$asc;
 
 		$this->columnSorts[] = $sort;
-		$this->sqlNeedsUpdate = TRUE;
 
 		return $this;
 
 	}
 
+	/**
+	 * executes query and returns array of Article instances
+	 *
+	 * @return array
+	 */
 	public function select() {
 
-		if($this->sqlNeedsUpdate) {
-			$this->buildQueryString();
-		}
-
+		$this->buildQueryString();
 		$this->buildValuesArray();
 
-		var_dump($this->sql);
+		//@todo do not prepare statement again, if query hasn't changed
+
+		$this->lastQuerySql = $this->sql;
 
 		$rows = $this->dbConnection->doPreparedQuery($this->sql, $this->valuesToBind);
 
-		var_dump($rows);
+		$ids = array();
 
+		foreach($rows as $row) {
+			$ids[] = $row['articlesID'];
+		}
+
+		return Article::getInstances($ids);
 	}
 
+	/**
+	 * adds LIMIT clause, executes query and returns array of Article instances
+	 *
+	 * @param number $rows
+	 * @return array
+	 */
 	public function selectFirst($rows = 1) {
 
+		$this->buildQueryString();
+		$this->buildValuesArray();
+
+		$this->sql .= " LIMIT $rows";
+
+		//@todo do not prepare statement again, if query hasn't changed
+
+		$this->lastQuerySql = $this->sql;
+
+		$rows = $this->dbConnection->doPreparedQuery($this->sql, $this->valuesToBind);
+
+		$ids = array();
+
+		foreach($rows as $row) {
+			$ids[] = $row['articlesID'];
+		}
+
+		return Article::getInstances($ids);
 	}
 
+	/**
+	 * static method for convenience reasons
+	 * avoids assigning ArticleQuery instance to variable before
+	 * specifying and executing query
+	 *
+	 * @param Mysqldbi $dbConnection
+	 * @return ArticleQuery
+	 */
+	public static function create(Mysqldbi $dbConnection) {
+		return new self($dbConnection);
+	}
+
+	/**
+	 * stores WHERE clause and values which must be bound
+	 *
+	 * @param string $conditionString
+	 * @param unknown $value
+	 */
 	private function addCondition($conditionString, $value) {
 
 		$condition = new \stdClass();
@@ -91,10 +185,11 @@ class ArticleQuery {
 
 		$this->whereClauses[] = $condition;
 
-		return $this;
-
 	}
 
+	/**
+	 * builds query string by parsing WHERE and ORDER BY clauses
+	 */
 	private function buildQueryString() {
 
 		$w = array();
@@ -118,6 +213,9 @@ class ArticleQuery {
 
 	}
 
+	/**
+	 * prepares array containing values which must be bound to prepared statement
+	 */
 	private function buildValuesArray() {
 
 		foreach($this->whereClauses as $where) {
