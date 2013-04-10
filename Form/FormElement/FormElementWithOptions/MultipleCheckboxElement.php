e826ce69e82949b4532d36f419c7abbd9768fdf3
gregor <git@gregorkofler.com>
2013-04-11 00:48:34 +0200

    Meet requirements of abstract class

--- Form/FormElement/FormElementWithOptions/MultipleCheckboxElement.php
+++ Form/FormElement/FormElementWithOptions/MultipleCheckboxElement.php
@@ -8,6 +8,11 @@ use vxPHP\Form\FormElement\FormElementWithOptions\FormElementWithOptions;
  * @todo implement stub
  */
 class MultipleCheckboxElement extends FormElementWithOptions {
+	public function createOptions($options) {
+
+	}
+
 	public function render() {
+
 	}
 }
