ecae57fd111b3756159b7eba7d37a10fd9bcc810
gregor <git@gregorkofler.com>
2013-10-20 23:38:04 +0200

    Anchor href and image src filter packaged into new class

---
+++ Template/Filter/AnchorHref.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace vxPHP\SimpleTemplate\Filter;
+
+use vxPHP\Webpage\Menu\Menu;
+use vxPHP\Http\Request;
+use vxPHP\Application\Application;
+
+/**
+ *
+ * @author Gregor Kofler
+ *
+ */
+class AnchorHref extends SimpleTemplateFilter implements SimpleTemplateFilterInterface {
+
+	/**
+	 * (non-PHPdoc)
+	 *
+	 * @see \vxPHP\SimpleTemplate\Filter\SimpleTemplateFilterInterface::parse()
+	 *
+	 */
+	public function apply(&$templateString) {
+
+		$templateString = preg_replace_callback(
+			'~<a(.*?)\s+href=("|\')\$([a-z0-9_]+[a-z0-9_.\/-]*)(.*?)\2(.*?)>~i',
+			array($this, 'filterHrefWithPath'),
+			$templateString
+		);
+
+		$templateString = preg_replace_callback(
+				'~<a(.*?)\s+href=("|\')\$\/([a-z0-9_.-]+)(.*?)\2(.*?)>~i',
+				array($this, 'filterHref'),
+				$templateString
+		);
+
+		$templateString = preg_replace_callback(
+			'~<img(.*?)\s+src=("|\')\$([^"\']+)\2(.*?)>~i',
+			array($this, 'filterImgSrc'),
+			$templateString
+		);
+
+	}
+
+	/**
+	 * callback to turn href shortcuts into site conform valid URLs
+	 * tries to build a path reflecting the position of the page in a nested menu
+	 *
+	 * $foo/bar?baz=1 becomes /level1/level2/foo/bar?baz=1 or index.php/level1/level2/foo/bar?baz=1
+	 *
+	 * @param array $matches
+	 * @return string
+	 */
+	private function filterHrefWithPath($matches) {
+
+		static $script;
+		static $niceUri;
+		static $menuEntryLookup = array();
+
+		$config = Application::getInstance()->getConfig();
+
+		if(is_null($script)) {
+			$script = trim(Request::createFromGlobals()->getScriptName(), '/');
+		}
+
+		if(empty($niceUri)) {
+			$niceUri = $config->site->use_nice_uris == 1;
+		}
+
+		$matchSegments = explode('/', $matches[3]);
+		$idToFind = array_shift($matchSegments);
+
+		$recursiveFind = function(Menu $m) use (&$recursiveFind, $idToFind) {
+
+			foreach($m->getEntries() as $e) {
+
+				if($e->getPage() === $idToFind) {
+					return $e;
+				}
+
+				if(($sm = $e->getSubMenu()) && $sm->getType() != 'dynamic') {
+					if($e = $recursiveFind($sm)) {
+						return $e;
+					}
+				}
+			}
+
+		};
+
+		if(isset($menuEntryLookup[$idToFind])) {
+			$e = $menuEntryLookup[$idToFind];
+		}
+
+		else {
+			foreach($config->menus as $menu) {
+
+				if($menu->getScript() !== $script) {
+					continue;
+				}
+
+				if($e = $recursiveFind($menu)) {
+					$menuEntryLookup[$idToFind] = $e;
+					break;
+				}
+
+			}
+		}
+
+		if(isset($e)) {
+
+			$pathSegments = array($e->getPage());
+
+			while($e = $e->getMenu()->getParentEntry()) {
+				$pathSegments[] = $e->getPage();
+			}
+
+			$uriParts = array();
+
+			if($niceUri) {
+				if($script !== 'index.php') {
+					$uriParts[] = basename($script, '.php');
+				}
+			}
+			else {
+				$uriParts[] = $script;
+			}
+			if(count($pathSegments)) {
+				$uriParts[] = implode('/', array_reverse($pathSegments));
+			}
+			if(count($matchSegments)) {
+				$uriParts[] = implode('/', $matchSegments);
+			}
+
+			$uri = implode('/', $uriParts) . $matches[4];
+
+			return "<a{$matches[1]} href={$matches[2]}/$uri{$matches[2]}{$matches[5]}>";
+
+		}
+	}
+
+	/**
+	 * callback to turn href shortcuts into site conform valid URLs
+	 *
+	 * $foo/bar?baz=1 becomes /foo/bar?baz=1 or index.php/foo/bar?baz=1
+	 *
+	 * @param array $matches
+	 * @return string
+	 */
+	private static function filterHref($matches) {
+
+		static $script;
+		static $niceUri;
+
+		if(empty($script)) {
+			$script = trim(Request::createFromGlobals()->getScriptName(), '/');
+		}
+
+		if(empty($niceUri)) {
+			$niceUri = Application::getInstance()->getConfig()->site->use_nice_uris == 1;
+		}
+
+		$matches[4] = html_entity_decode($matches[4]);
+
+		$uriParts = array();
+
+		if($niceUri) {
+			if($script !== 'index.php') {
+				$uriParts[] = basename($script, '.php');
+			}
+		}
+		else {
+			$uriParts[] = $script;
+		}
+
+		if($matches[3] !== '') {
+			$uriParts[] = $matches[3];
+		}
+
+		$uri = implode('/', $uriParts) . $matches[4];
+
+		return "<a{$matches[1]} href={$matches[2]}/$uri{$matches[2]}{$matches[5]}>";
+	}
+
+	/**
+	 * callback to turn src attribute of image tag to match ressources in IMG_SITE_PATH folder
+	 *
+	 * @param array $matches
+	 * @return string
+	 */
+	private static function filterImgSrc($matches) {
+
+		return "<img{$matches[1]} src={$matches[2]}".IMG_SITE_PATH."{$matches[3]}{$matches[2]}{$matches[4]}>";
+
+	}
+}
