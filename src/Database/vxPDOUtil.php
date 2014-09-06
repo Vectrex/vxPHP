8938c13c5d3941f2b2ca07f8e7f36b6a486dcd4e
gregor <info@gregorkofler.com>
2014-09-06 21:06:23 +0200

    unFormatDate() and unFormatDecimal() implemented; unFormatDate() and unFormatDecimal() replace formatDate() and formatDecimal() respectively; customSort() was removed, since it wasn't used in vxWeb anymore and was way to specific even for a utility class

--- src/Database/vxPDOUtil.php
+++ src/Database/vxPDOUtil.php
@@ -16,16 +16,98 @@ namespace vxPHP\Database;
  */
 class vxPDOUtil {
 	
-	public static function customSort() {
+	/**
+	 * Re-formats a date strings depending on a supplied input locale to yyyy-mm-dd
+	 * does not check validity of date
+	 * supported locales are "us" | "iso" | "de"
+	 * if no locale is provided, strtotime() tries to interpret the date string
+	 * an empty string is returned if date string could not be parsed
+	 *
+	 * @param string $dateString
+	 * @param string $locale
+	 *
+	 * @return string
+	 */
+	public static function unFormatDate($dateString, $locale = '') {
+	
+		if(empty($dateString)) {
+			return '';
+		}
+	
+		$tmp = preg_split('/( |\.|\/|-)/', $dateString);
+
+		if(count($tmp) === 3) {
+
+			switch($locale) {
+	
+				case 'de':
+					$tmp[2] = substr(date('Y'), 0, 4 - strlen($tmp[2])).$tmp[2];
+					return sprintf('%04d-%02d-%02d', $tmp[2], $tmp[1], $tmp[0]);
+	
+				case 'us':
+					$tmp[2] = substr(date('Y'), 0, 4 - strlen($tmp[2])).$tmp[2];
+					return sprintf('%04d-%02d-%02d', $tmp[2], $tmp[0], $tmp[1]);
+	
+				case 'iso':
+					$tmp[0] = substr(date('Y'), 0, 4 - strlen($tmp[0])).$tmp[0];
+					return sprintf('%04d-%02d-%02d', $tmp[0], $tmp[1], $tmp[2]);
+	
+				default:
+					if(($parsed = strtotime($dateString)) === FALSE) {
+						return '';
+					}
+					return date('Y-m-d', $parsed);
+			}
+		}
+
+		if(($parsed = strtotime($dateString)) === FALSE) {
+			return '';
+		}
+
+		return date('Y-m-d', $parsed);
+
 	}
 	
-	public static function formatDate() {
-	
+	/**
+	 * Strips decimal strings from everything but decimal point and negative prefix
+	 * and returns the result as float
+	 * 
+	 * @param string $decimalString
+	 * @return float | NaN
+	 */
+	public static function unFormatDecimal($decimalString) {
+
+		if(trim($decimalString) == '') {
+			return NAN;
+		}
+
+		// remove a leading "+"
+
+		$decimalString = rtrim('+', trim($decimalString));
+
+		// only a decimal separator ("," or ".")
+
+		if(preg_match('/^\-?\d+([,.]\d+)?$/', $decimalString)) {
+			return (float) (str_replace(',', '.', $decimalString));
+		}
+		
+		// "," as thousands separator "." as decimal separator
+
+		if(preg_match('/^\-?[1-9]\d{0,2}((,|\')\d{3})*(\.\d+)?$/', $decimalString)) {
+			return (float) (str_replace(array(',', '\''), array('', ''), $decimalString));
+		}
+		
+		// "." as thousands separator "," as decimal separator
+		
+		if(preg_match('/^\-?[1-9]\d{0,2}(\.\d{3})*(,\d+)?$/', $decimalString)) {
+			return (float) (str_replace(array('.', ','), array('', '.'), $decimalString));
+		}
+		
+		// try type casting
+
+		return (float) $decimalString;
 	}
 	
-	public static function formatDecimal() {
-	
-	}
 	
 	/**
 	 * checks whether $aliasText in $column is already present
