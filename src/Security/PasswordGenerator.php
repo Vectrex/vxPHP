277aee0e764f8e2a27aa82b9e2f0f816a1c1a79e
Gregor Kofler <info@gregorkofler.com>
2016-07-20 18:34:00 +0200

    Updated and relocated password generator; on PHP7 the more secure random_int() function is used; minor code cleanups

--- src/Security/PasswordGenerator.php
+++ src/Security/PasswordGenerator.php
@@ -9,7 +9,7 @@
  */
 
 
-namespace vxPHP\Util;
+namespace vxPHP\Security;
 
 /**
  * Create passwords
@@ -17,7 +17,7 @@ namespace vxPHP\Util;
  * @author Martin Jansen,  Olivier Vanhoucke
  * @author Gregor Kofler
  * 
- * @version 1.0.4 2015-12-09
+ * @version 1.1.0 2016-07-20
  */
 
 class PasswordGenerator {
@@ -26,18 +26,20 @@ class PasswordGenerator {
 	*
 	* @param  integer Length of the password.
 	* @param  string  Type of password (pronounceable, unpronounceable)
-	* @param  string  Character which could be use in the
-	*                 unpronounceable password ex : 'A,B,C,D,E,F,G'
+	* @param  string  Character which can be used in the
+	*                 unpronounceable password e.g. : 'A,B,C,D,E,F,G'
 	*                 or numeric or alphanumeric.
 	* @return string  Returns the generated password.
 	*/
 
 	public static function create($length = 10, $type = 'pronounceable', $chars = '') {
+
 		mt_srand();
 
 		switch ($type) {
 			case 'unpronounceable' :
 				return self::createUnpronounceable($length, $chars);
+
 			case 'pronounceable' :
 			default :
 				return self::createPronounceable($length);
@@ -53,15 +55,17 @@ class PasswordGenerator {
 	* @param  integer Number of different password
 	* @param  integer Length of the password
 	* @param  string  Type of password (pronounceable, unpronounceable)
-	* @param  string  Character which could be use in the
+	* @param  string  Character which can be used in the
 	*                 unpronounceable password ex : 'A,B,C,D,E,F,G'
 	*                 or numeric or alphanumeric.
 	* @return array   Array containing the passwords
 	*/
 	public static function createMultiple($number, $length = 10, $type = 'pronounceable', $chars = '') {
-		$passwords = array();
 
-		while ($number > 0) {
+		$passwords = [];
+
+		while ($number--) {
+
 			while(true) {
 				$password = self::create($length, $type, $chars);
 				if (!in_array($password, $passwords)) {
@@ -69,8 +73,9 @@ class PasswordGenerator {
 					break;
 				}
 			}
-			$number--;
+
 		}
+
 		return $passwords;
 	}
 
@@ -85,6 +90,7 @@ class PasswordGenerator {
 	* @return string
 	*/
 	public static function createFromLogin($login, $type, $key = 0) {
+
 		switch($type) {
 			case 'reverse':			return strrev($login);
 			case 'shuffle':			return self::shuffle($login);
@@ -100,9 +106,9 @@ class PasswordGenerator {
 	}
 
 	/**
-	* Create multiple, different passwords from an array of login
+	* Create multiple, different passwords from an array of logins
 	*
-	* Method to create a list of different password from login
+	* Method to create a list of different passwords from logins
 	*
 	* @param  array   Login
 	* @param  string  Type
@@ -110,76 +116,90 @@ class PasswordGenerator {
 	* @return array   Array containing the passwords
 	*/
 	public static function createMultipleFromLogin($login, $type, $key = 0) {
-		$passwords = array();
-		$number    = count($login);
-		$save      = $number;
 
-		while ($number > 0) {
+		$passwords = [];
+		$number    = count($login);
+
+		while ($number--) {
+
+			$loginName = array_shift($login);
+
 			while (true) {
-				$password = self::createFromLogin($login[$save - $number], $type, $key);
+				$password = self::createFromLogin($loginName, $type, $key);
+
 				if (!in_array($password, $passwords)) {
 					$passwords[] = $password;
 					break;
 				}
 			}
-			$number--;
+
 		}
+
 		return $passwords;
 	}
 
 	private static function eXor($login, $key) {
+
 		$tmp = '';
 
-		for ($i = 0; $i < strlen($login); $i++) {
+		for ($i = 0; $i < strlen($login); ++$i) {
 			$next = ord($login{$i}) ^ $key;
 			if		($next > 255)	{ $next -= 255; }
 			elseif	($next < 0)		{ $next += 255;	}
 			$tmp .= chr($next);
 		}
 		return $tmp;
+
 	}
 
 	private static function asciiRotx($login, $key) {
+
 		$tmp = '';
 
-		for ($i = 0; $i < strlen($login); $i++) {
+		for ($i = 0; $i < strlen($login); ++$i) {
 			$next = ord($login{$i}) + $key;
 			if		($next > 255)	{ $next -= 255; }
 			elseif	($next < 0)		{ $next += 255;	}
 			$tmp .= chr($next);
 		}
 		return $tmp;
+
 	}
 
 	private static function asciiRotxpp($login, $key) {
+
 		$tmp = '';
 
-		for ($i = 0; $i < strlen($login); $i++, $key++) {
+		for ($i = 0; $i < strlen($login); ++$i, ++$key) {
 			$next = ord($login{$i}) + $key;
 			if		($next > 255)	{ $next -= 255; }
 			elseif	($next < 0)		{ $next += 255;	}
 			$tmp .= chr($next);
 		}
 		return $tmp;
+
 	}
 
 	private static function asciiRotxmm($login, $key) {
+
 		$tmp = '';
 
-		for ($i = 0; $i < strlen($login); $i++, $key--) {
+		for ($i = 0; $i < strlen($login); $i++, --$key) {
 			$next = ord($login{$i}) + $key;
 			if		($next > 255)	{ $next -= 255; }
 			elseif	($next < 0)		{ $next += 255;	}
 			$tmp .= chr($next);
 		}
 		return $tmp;
+
 	}
 
 	private static function rotx($login, $key) {
+
 		$tmp = '';
 		$login = strtolower($login);
 
-		for ($i = 0; $i < strlen($login); $i++) {
+		for ($i = 0; $i < strlen($login); ++$i) {
 			if ((ord($login{$i}) >= 97) && (ord($login{$i}) <= 122)) { // 65, 90 for uppercase
 				$next = ord($login{$i}) + $key;
 				if		($next > 122)	{ $next -= 26; }
@@ -191,13 +211,15 @@ class PasswordGenerator {
 			}
 		}
 		return $tmp;
+
 	}
 
 	private static function rotxpp($login, $key) {
+
 		$tmp = '';
 		$login = strtolower($login);
 
-		for ($i = 0; $i < strlen($login); $i++, $key++) {
+		for ($i = 0; $i < strlen($login); ++$i, ++$key) {
 			if ((ord($login{$i}) >= 97) && (ord($login{$i}) <= 122)) { // 65, 90 for uppercase
 				$next = ord($login{$i}) + $key;
 				if		($next > 122)	{ $next -= 26; }
@@ -209,13 +231,15 @@ class PasswordGenerator {
 			}
 		}
 		return $tmp;
+
 	}
 
 	private static function rotxmm($login, $key) {
+
 		$tmp = '';
 		$login = strtolower($login);
 
-		for ($i = 0; $i < strlen($login); $i++, $key--) {
+		for ($i = 0; $i < strlen($login); ++$i, --$key) {
 			if ((ord($login{$i}) >= 97) && (ord($login{$i}) <= 122)) { // 65, 90 for uppercase
 				$next = ord($login{$i}) + $key;
 				if		($next > 122)	{ $next -= 26; }
@@ -227,16 +251,19 @@ class PasswordGenerator {
 			}
 		}
 		return $tmp;
+
 	}
 
 	private static function shuffle($login) {
-		$tmp = array();
 
-		for ($i = 0; $i < strlen($login); $i++) {
+		$tmp = [];
+
+		for ($i = 0; $i < strlen($login); ++$i) {
 			$tmp[] = $login{$i};
 		}
 		shuffle($tmp);
 		return implode($tmp, '');
+
 	}
 
 	/**
@@ -249,18 +276,50 @@ class PasswordGenerator {
 	* @return string  Returns the password
 	*/
 	private static function createPronounceable($length) {
+
 		$retVal = '';
 
-		$v = array('a', 'e', 'i', 'o', 'u', 'ae', 'ou', 'io', 'ea', 'ou', 'ia', 'ai', 'ei');
+		$v = [
+				'a', 'e', 'i', 'o',
+				'u', 'ae', 'ou', 'io',
+				'ea', 'ou', 'ia', 'ai',
+				'ei'
+		];
 
-		$c = array('b', 'c', 'd', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'u', 'v', 'w', 'z',
-			'tr', 'cr', 'fr', 'dr', 'wr', 'pr', 'th', 'ch', 'ph', 'st', 'sl', 'cl', 'br');
+		$c = [
+				'b', 'c', 'd', 'g',
+				'h', 'j', 'k', 'l',
+				'm', 'n', 'p', 'r',
+				's', 't', 'u', 'v',
+				'w', 'z', 'tr', 'cr',
+				'fr', 'dr', 'wr', 'pr',
+				'th', 'ch', 'ph', 'st',
+				'sl', 'cl', 'br'
+		];
 
-		$lenV = count($v)-1;
-		$lenC = count($c)-1;
-		for ($i = 0; $i < $length; $i++) {
-			$retVal .= $c[mt_rand(0, $lenC)] . $v[mt_rand(0, $lenV)];
+		$lenV = count($v) - 1;
+		$lenC = count($c) - 1;
+
+		// use the cryptographically secure random number generator
+		
+		if(function_exists('random_int')) {
+
+			for ($i = 0; $i < $length; ++$i) {
+				$retVal .= $c[random_int(0, $lenC)] . $v[random_int(0, $lenV)];
+			}
+				
 		}
+
+		// use the less secure fallback vor older PHP versions
+
+		else {
+
+			for ($i = 0; $i < $length; ++$i) {
+				$retVal .= $c[mt_rand(0, $lenC)] . $v[mt_rand(0, $lenV)];
+			}
+
+		}
+
 		return substr($retVal, 0, $length);
 	}
 
@@ -270,31 +329,62 @@ class PasswordGenerator {
 	* This method creates a random unpronounceable password
 	*
 	* @param  integer Length of the password
-	* @param  string  Character which could be use in the
+	* @param  string  Character which could be used in the
 	*                 unpronounceable password ex : 'A,B,C,D,E,F,G'
 	*                 or numeric or alphanumeric.
 	* @return string  Returns the password
 	*/
 	private static function createUnpronounceable($length, $chars) {
+
 		$password = '';
 
 		switch($chars) {
 			case 'alphanumeric':
-				$regex = '[a-z0-9]'; break;
+				$regex = '[a-z0-9]';
+				break;
+
 			case 'numeric':
-				$regex = '[0-9]'; break;
+				$regex = '[0-9]';
+				break;
+
 			case '':
-				$regex = '[a-z0-9_#@%&]'; break;
+				$regex = '[a-z0-9_#@%&]';
+				break;
+
 			default:
-				$chars = str_replace(array('+', '|', '$', '^', '/', '\\', ), '' , trim($chars));
-				$chars = str_replace(',,', ',', $chars);
-				if ($chars{strlen($chars)-1} == ',') { $chars = substr($chars, 0, -1); }
+				$chars = str_replace(',,', ',', str_replace(['+', '|', '$', '^', '/', '\\', ], '' , trim($chars)));
+				if ($chars{strlen($chars)-1} == ',') {
+					$chars = substr($chars, 0, -1);
+				}
 				$regex = str_replace(',', '|', $chars);
 		}
 
+		// use the cryptographically secure random number generator
+		
+		if(function_exists('random_int')) {
+		
+			do {
+				$chr = chr(random_int(32, 127));
+
+				if (preg_match('/' . $regex . '/USi', $chr)) {
+					$password .= $chr;
+				}
+
+			} while (strlen($password) < $length);
+			
+			return $password;
+
+		}
+		
+		// use the less secure fallback vor older PHP versions
+
 		do {
-			$chr = chr(mt_rand(0, 255));
-			if (preg_match('/'.$regex.'/USi', $chr)) { $password .= $chr; }
+			$chr = chr(mt_rand(32, 127));
+
+			if (preg_match('/'.$regex.'/USi', $chr)) {
+				$password .= $chr;
+			}
+
 		} while (strlen($password) < $length);
 
 		return $password;
