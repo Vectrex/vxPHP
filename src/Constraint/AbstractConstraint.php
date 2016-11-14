2f578bd9061e05317e959ba6e4c38b4867faf3f9
Gregor Kofler <info@gregorkofler.com>
2016-11-14 18:30:37 +0100

    Added setErrorMessage() and getErrorMessage() methods to constraint

--- src/Constraint/AbstractConstraint.php
+++ src/Constraint/AbstractConstraint.php
@@ -17,9 +17,17 @@ use vxPHP\Constraint\ConstraintInterface;
  * 
  * @author Gregor Kofler, info@gregorkofler.com
  * 
- * @version 0.1.0, 2016-10-31
+ * @version 0.2.0, 2016-11-14
  */
 abstract class AbstractConstraint implements ConstraintInterface {
+
+	/**
+	 * error message giving a description of the constraint violation
+	 * set by a validator subclass
+	 * 
+	 * @var string
+	 */
+	protected $errorMessage;
 	
 	/**
 	 *
@@ -28,5 +36,36 @@ abstract class AbstractConstraint implements ConstraintInterface {
 	 * @see \vxPHP\Constraint\ConstraintInterface::validate()
 	 */
 	public abstract function validate($value);
+	
+	/**
+	 * get error message
+	 * 
+	 * @return string
+	 */
+	public function getErrorMessage() {
+		
+		return $this->errorMessage;
+		
+	}
 
+	/**
+	 * set error message
+	 * 
+	 * @param string $message
+	 */
+	protected function setErrorMessage($message) {
+		
+		$this->errorMessage = $message;
+
+	}
+
+	/**
+	 * clear error message
+	 * called by a validator prior to starting a validation
+	 */
+	protected function clearErrorMessage() {
+	
+		$this->errorMessage = '';
+	
+	}
 }
