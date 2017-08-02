1bed7973fbe383d63faaecacbc39111fbebcddb3
Gregor Kofler <info@gregorkofler.com>
2017-08-02 23:26:32 +0200

    Cleaned up code of ShortenText filter; added a simple Spaceless filter

--- src/Template/Filter/Spaceless.php
+++ src/Template/Filter/Spaceless.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of the vxPHP/vxWeb framework
  *
@@ -7,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 
 namespace vxPHP\Template\Filter;
 
@@ -19,31 +19,17 @@ namespace vxPHP\Template\Filter;
  *
  * @author Gregor Kofler
  */
-class ShortenText extends SimpleTemplateFilter implements SimpleTemplateFilterInterface {
+class Spaceless extends SimpleTemplateFilter implements SimpleTemplateFilterInterface {
 
 	/**
 	 * {@inheritDoc}
 	 * @see \vxPHP\Template\Filter\SimpleTemplateFilter::apply()
 	 */
 	public function apply(&$templateString) {
-
-		$templateString = preg_replace_callback(
-			'~<(\w+)\s+(.*?)class=(\'|")?([a-z0-9_]*\s*)shortened_(\d+)(.*?)>(?s)(.*?)(?-s)</\s*\1>~i',
-			function($matches) {
-				$prefix = sprintf("<%s %sclass=%s%sshortened%s>", $matches[1], $matches[2], $matches[3], $matches[4], $matches[6]);
-				$suffix = sprintf("</%s>", $matches[1]);
-				$len = (int) $matches[5];
-				$src = $matches[7];
-				
-				if(strlen(strip_tags($src)) <= $len) {
-					return $prefix . $src . $suffix;
-				}
-				
-				$ret = substr(strip_tags($src), 0, $len + 1);
-				return $prefix . substr($ret,0,strrpos($ret,' ')) . '&hellip;' . $suffix;
-			},
-			$templateString
-		);
-
+		
+		$templateString = trim(preg_replace('/>\s+</', '><', $templateString));
+		
 	}
+
 }
+
