b9f5f13d6e05098a384741b3cacfa2e3b0c56424
Gregor Kofler <info@gregorkofler.com>
2018-04-25 16:51:40 +0200

    Autoloader for tests updated

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,15 @@
 <?php
-
+/*
 if (!@include_once __DIR__ . '/../vendor/autoload.php') {
 	if (!@include_once __DIR__ . '/../../../autoload.php') {
 		trigger_error("Unable to load dependencies", E_USER_ERROR);
 	}
 }
+*/
 
+require_once __DIR__ . '/../src/Autoload/Psr4.php';
+
+$loader = new vxPHP\Autoload\Psr4();
+$loader->register();
+
+$loader->addPrefix('vxPHP', __DIR__ . '/../src');
