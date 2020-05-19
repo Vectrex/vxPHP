62f0675bea9ad307341d0bd57a7513fa470eee17
Gregor <info@gregorkofler.com>
2020-05-20 00:08:21 +0200

    Added a few more simple tests

--- tests/Templating/SimpleTemplateTest.php
+++ tests/Templating/SimpleTemplateTest.php
@@ -10,7 +10,13 @@ class SimpleTemplateTest extends TestCase
 {
     public function testAssign()
     {
-        $this->assertTrue(true);
+        $template = new SimpleTemplate();
+        $template->setRawContents('<p><?= $this->date->format("Y-m-d") ?></p>');
+        $template->assign('date', new \DateTime());
+        $this->assertEquals('<p>' . (new \DateTime())->format('Y-m-d') . '</p>', $template->display([]));
+        $template->setRawContents('<?= $this->a ?>#<?= $this->b ?>#<?= $this->c ?>#<?= $this->a ?>');
+        $template->assign(['a' => 'A', 'b' => 'B', 'c' => 'C']);
+        $this->assertEquals('A#B#C#A', $template->display([]));
     }
 
     public function testAssignString()
@@ -19,5 +25,27 @@ class SimpleTemplateTest extends TestCase
         $template->setRawContents('<div><?= $this->text ?></div>');
         $template->assignString('text', '<p>a & o</p>');
         $this->assertEquals('<div>&lt;p&gt;a &amp; o&lt;/p&gt;</div>', $template->display([]));
+    }
+
+    public function testContainsPhp ()
+    {
+        $template = new SimpleTemplate();
+        $template->setRawContents('<p><?php echo "Check"; ?></p>');
+        $this->assertTrue($template->containsPHP());
+        $template->setRawContents('<p><?= (new DateTime())->format("Y-m-d") ?></p>');
+        $this->assertTrue($template->containsPHP());
+        $template->setRawContents('<p>Check</p>');
+        $this->assertFalse($template->containsPHP());
+    }
+
+    public function testGetParentTemplateName ()
+    {
+        $template = new SimpleTemplate();
+        $template->setRawContents('<div><!-- {extend: parent.php@content }--></div>');
+        $this->assertEquals('parent.php', $template->getParentTemplateFilename());
+        $template = new SimpleTemplate();
+        $template->setRawContents('<!-- a comment --><div><!-- {extend: path_to/parent.php @ content }-->');
+        $this->assertEquals('path_to/parent.php', $template->getParentTemplateFilename());
+
     }
 }
